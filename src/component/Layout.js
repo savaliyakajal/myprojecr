b6cf6cdc48a61975c2f4b7687aad5d7184175121
savaliyakajal <kajal.panthercodx@gamil.com>
2023-04-07 14:38:40 +0530

    mainapidata

--- src/component/Layout.js
+++ src/component/Layout.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function Layout(props) {
-    return (
-        <div>
-            {props.children}
-        </div>
-    );
+  return <div>{props.children}</div>;
 }
 
 export default Layout;
